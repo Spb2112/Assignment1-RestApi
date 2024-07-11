0955d595faba899207d760b8cefb0a67ab618f43
Spb2112 <155693565+Spb2112@users.noreply.github.com>
2024-07-11 17:47:59 -0600

    Add files via upload

---
+++ CustomerAPI/CustomerAPI/Models/CustomerRepo.cs
@@ -0,0 +1,17 @@
+﻿using CustomerAPI.Interfaces;
+
+namespace CustomerAPI.Models
+{
+    public class CustomerRepo : ICustomerRepo
+    {
+        public List<Customer> GetAllCustomers()
+        {
+            return new List<Customer>
+        {
+            new Customer { Id = 1, FirstName = "sourav", LastName = "Bhatt" },
+            new Customer { Id = 2, FirstName = "prachi", LastName = "Bhatt" },
+
+        };
+        }
+    }
+}
